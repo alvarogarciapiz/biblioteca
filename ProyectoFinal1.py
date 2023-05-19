235c4017bf0b8e29c144f0bef5d9154674b7207b
Álvaro García Pizarro <81751965+alvarogarciapiz@users.noreply.github.com>
2023-05-19 18:43:59 +0200

    Update ProyectoFinal1.py

--- ProyectoFinal1.py
+++ ProyectoFinal1.py
@@ -1,7 +1,7 @@
 '''
     Proyecto final biblioteca - Álvaro García Pizarro
     27 de abril de 2023
-    Formación Banco Santander - Python
+    Formación Banco Santander - Python2
 '''
 #Librerías necesarias
 import csv
